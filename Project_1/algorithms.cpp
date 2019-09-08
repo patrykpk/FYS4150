006a76e9a9db989df11d728725076d280c10dbc8
patrykpk <patrykpk@users.noreply.github.com>
2019-09-08 20:19:29 +0200

    Added comments and formatting for the output file

--- Project_1/algorithms.cpp
+++ Project_1/algorithms.cpp
@@ -8,11 +8,14 @@
 using namespace arma;
 
 void LU_Decomp_Arma(double *x, double (*f)(double), double h, int n){
+    // This function uses Armadillo
 
-    mat A = zeros<mat>(n,n);
-    vec d_LU(n);
+    mat A = zeros<mat>(n,n);  //Creating a n x n matrix filled with zeros
+    vec d_LU(n);              //Array on the right-hand side
 
     clock_t start, finish;
+
+    // Setting up the matrix and the right-hand side vector with correct values
     A(0,0) = 2.0;  A(0,1) = -1;   d_LU(0) =  h*h*f(x[1]);
     d_LU(n-1) = h*h*f(x[n]);
     for (int i = 1; i < n-1; i++){
@@ -23,11 +26,12 @@ void LU_Decomp_Arma(double *x, double (*f)(double), double h, int n){
     }
     A(n-1,n-1) = 2.0; A(n-2,n-1) = -1.0; A(n-1,n-2) = -1.0;
 
-    // solve Av = d
-    start = clock();
+    // Solving Av = d
+    start = clock();     // Starting the timer
     vec v_LU  = solve(A,d_LU);
-    finish = clock();
+    finish = clock();    // Stopping the timer
 
+    // Calculates time used for the loop
     double time_LU = (double) (finish - start)/(CLOCKS_PER_SEC);
     printf("--------------------------------------------------\n");
     printf("Performing LU required: %.8fs\n", time_LU);
@@ -36,43 +40,54 @@ void LU_Decomp_Arma(double *x, double (*f)(double), double h, int n){
 
 
 double *General_Algorithm(double *x, double (*f)(double), double h, int n){
-    double *d = new double[n+1];
-    int *a = new int[n+1];
-    int *b = new int[n+1];
-    int *c = new int[n+1];
 
-    double *b_tilde = new double[n+1];
-    double *v = new double[n+2]; // Numerical solution
-    double *d_tilde = new double[n+1]; // Vector on right hand side updated
+    //Dynamic allocation of memory for arrays
+    int *a = new int[n+1];        // Elements below the diagonal
+    int *b = new int[n+1];        // Diagonal elements
+    int *c = new int[n+1];        // Elements above the diagonal
+    double *d = new double[n+1];  // Right-hand side
 
-    v[0] = 0;
+    double *b_tilde = new double[n+1];  // Updated diagonal elements
+    double *d_tilde = new double[n+1];  // Vector on right hand side updated
+    double *v = new double[n+2];        // Numerical solution
+
+    // Setting up arrays
     for (int i=1; i<=n; i++) {
         d[i] = h*h*f(x[i]);
         b[i] = 2;
         a[i] = -1;
         c[i] = -1;
     }
-    d[0] = 0;
-    c[n] = 0;
+    d[0] = 0;  //Filling in a spot for one value
+
+    // Arrays below and above diagonal contain one less value
+    c[n] = 0; // Overwriting the value
     a[1] = 0;
 
 
     clock_t start, finish;
     b_tilde[1] = b[1];
     d_tilde[1] = d[1];
-    start = clock();
+
+    // Forward substitution - Row reduction
+    start = clock();     // Starting the timer
     for (int i=2;i<=n;i++) {
-       b_tilde[i] = b[i] - (a[i]*c[i-1])/b_tilde[i-1];
-       d_tilde[i] = d[i]-(d_tilde[i-1]*a[i])/b_tilde[i-1];
+       b_tilde[i] = b[i] - (a[i]*c[i-1])/b_tilde[i-1];       // Updating the diagonal elements
+       d_tilde[i] = d[i]-(d_tilde[i-1]*a[i])/b_tilde[i-1];   // Updating the right hand side
     }
 
-    v[n] = (d_tilde[n])/b_tilde[n];
-    v[0] = 0;
+    v[0] = 0;    // Boundary condition
+    v[n+1] = 0;  // Boundary condition
+    v[n] = (d_tilde[n])/b_tilde[n];   // Precalculating
+
+    // Backward substitution
     for (int i=n;i>1;i--) {
         v[i-1] = (d_tilde[i-1]-c[i-1]*v[i])/b_tilde[i-1];
     }
 
-    finish = clock();
+    finish = clock();  // Stopping the timer
+
+    // Calculates time used for the loop
     double time_general = (double) (finish - start)/((double) CLOCKS_PER_SEC);
     printf("/////////////////////////////////////////////////////////////\n");
     printf("Checking time while performing general and special algorithm:\n");
@@ -80,48 +95,59 @@ double *General_Algorithm(double *x, double (*f)(double), double h, int n){
     printf("\n");
     printf("--------------------------------------------------\n");
     printf("Performing general algorithm required: %.8fs\n", time_general);
+
+    // Freeing the memory for arrays
     delete [] d;
     delete [] a;
     delete [] b;
     delete [] c;
     delete [] d_tilde;
 
-    return v;
+    return v;  // Returning the numerical solution
 
 }
 
 double *Special_Algorithm(double *x, double (*f)(double), double h, int n){
-    double *d = new double[n+1];
-    double *b_special = new double[n+1]; // Diagonal element
+
+    double *b_special = new double[n+1]; // Diagonal elements
     double *v_special = new double[n+2]; // Numerical solution
+    double *d = new double[n+1];         // Vector on the right hand side
     double *d_special = new double[n+1]; // Vector on right hand side updated
     clock_t start, finish;
+
+    // Setting up the right hand side and diagonal elements
     for (int i = 1; i <= n; i++) {
         b_special[i] = (i+1.0)/((double) i);
         d[i] = h*h*f(x[i]);
     }
-    v_special[0] = 0;
-    v_special[n+1] = 0;
+    v_special[0] = 0;      // Boundary condition
+    v_special[n+1] = 0;    // Boundary condition
     d_special[1] = h*h*f(x[1]);
-    start = clock();
+
+    //Updating the right hand side
     for (int i = 2; i <= n; i++) {
         d_special[i] = d[i] + ((i-1.0)*d_special[i-1])/((double) i);
-    }
+   }
 
-   v_special[n] = d_special[n]/b_special[n];
+   v_special[n] = d_special[n]/b_special[n];  // Precalculated
+
+   // Backward substitution
+   start = clock();      // Starting the timer
    for (int i = n; i >= 2; i--) {
    v_special[i-1] = ((i-1.0)/((double)i)) * (d_special[i-1]+v_special[i]);
    }
+   finish = clock();    // Stopping the timer
 
-   finish = clock();
+   // Calculates time used for the loop
    double time_special = (double) (finish - start)/((double) CLOCKS_PER_SEC);
    printf("Performing special algorithm required: %.8fs\n", time_special);
    printf("--------------------------------------------------\n");
 
+   // Freeing the memory for arrays
    delete [] b_special;
    delete [] d_special;
    delete [] d;
 
 
-   return v_special;
+   return v_special;  // Returning the numerical solution
 }
--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -1,7 +1,7 @@
-# include <iostream>
-# include <fstream>
-# include <iomanip>
-# include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
 #include <string>
 #include <armadillo>
 #include "time.h"
@@ -12,68 +12,80 @@ using namespace arma;
 using namespace std;
 ofstream ofile;
 
+//Functions
+double Solution(double x) {return 1.0-(1.0-exp(-10.0))*x-exp(-10.0*x);}   // Known solution
 
-double Solution(double x) {return 1.0-(1.0-exp(-10.0))*x-exp(-10.0*x);}
+double f(double x) {return 100*exp(-10*x);}   // Source term
 
-double f(double x) {return 100*exp(-10*x);}
-
-
+//Main program starts here
 int main(int argc, char* argv[]) {
 
     char *outfilename;
     int n;
 
+    // Checking if enough arguments are given in the command line
     if( argc <= 2 ){
       cout << "Error occured: " << argv[0] <<
-          " read also output filename, n (int) and if you want to run LU on same line" << endl;
+          "Required arguments are the output filename without extension and a value of n (int)" << endl;
+      cout << "Optional: Inserting an arbitary third argument will also run LU" << endl;
       exit(1);
     }
     else{
-      outfilename = argv[1];
-      n = atoi(argv[2]);
-      char* Add_Amount = argv[2];
-      string str = ".txt";
+      outfilename = argv[1];          // First command line argument - Filename
+      n = atoi(argv[2]);              // Second command line argument - Value of n
+
+      char* Add_Amount = argv[2];     // Pointer to the n value
+      string str = ".txt";            // Extension to the file
       char* Extension = &str[0];
 
-      strcat(outfilename,Add_Amount);
-      strcat(outfilename, Extension);
+      strcat(outfilename,Add_Amount);  // Adding the n value to the output name
+      strcat(outfilename, Extension);  // Adding the extension to the output name
+
+      // Resulting filename "outfilenamen.txt"
     }
 
-    double h = 1.0/(n+1.0);
-    double *x = new double[n+2];
-    double *u = new double[n+2]; // Analytical solution
+    double h = 1.0/(n+1.0);            // Step length
 
+    //Dynamic allocation of memory
+    double *x = new double[n+2];      // Grid points
+    double *u = new double[n+2];      // Analytical solution
+
+    // Filling the arrays with values
     for (int i=0; i<=n+1; i++) {
         x[i] = i*h;
         u[i] = Solution(x[i]);
     }
-    u[0] = 0;
-    u[n+1] = 0;
+    u[0] = 0;    // Making sure that the boundary conditions
+    u[n+1] = 0;  // are satisfied
 
+    // Acquiring the values for general algorithm
     double *v = new double[n+2];
-    v = General_Algorithm(x, f, h, n);
+    v = General_Algorithm(x, f, h, n);           // Function from algorithms.cpp
 
+    //Acquiring the values for special algorithm
     double *v_special = new double[n+2];
-    v_special = Special_Algorithm(x, f, h, n);
+    v_special = Special_Algorithm(x, f, h, n);   // Function from algorithms.cpp
 
+    // Checking if third command line argument is given
     if (argv[3]){
-        cout << "Running LU" << "\n";
-        LU_Decomp_Arma(x, f, h, n);
+        cout << "Running LU" << endl;
+        LU_Decomp_Arma(x, f, h, n);              // Function from algorithms.cpp
     } else if (!argv[3]){
-        cout << "--------------"<< "\n";
+        cout << "--------------"<< endl;
         cout << "Not running LU" << endl;
-        cout << "--------------"<< "\n";
-        cout << "If you want to run LU decomposition enter an arbitrary third argument,\nbut keep in mind that the program will crash at n = 10^5" << "\n";
-        //exit(1);
+        cout << "--------------"<< endl;
+        cout << "If you want to run LU decomposition enter an arbitrary third argument,\n"
+                "but keep in mind that the program will crash at n = 10^5" << endl;
     }
 
     // Open file and write results to file:
     ofile.open(outfilename);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
-    ofile << "       x:             u(x):          v(x):          v_special         RelativeError_General        RelativeError_Special" << endl;
+    ofile << "         x:             u(x):         v(x):       v_special:   RelError_Gen:  RelError_Spec:" << endl;
     for (int i=0;i<=n+1;i++) {
-      double RelativeError_S = log10(fabs((u[i]-v_special[i])/u[i]));
-      double RelativeError_G = log10(fabs((u[i]-v[i])/u[i]));
+      double RelativeError_S = log10(fabs((u[i]-v_special[i])/u[i]));  // Calculating the relative error for special case
+      double RelativeError_G = log10(fabs((u[i]-v[i])/u[i]));          // Calculating the relative error for general case
+
        ofile << setw(15) << setprecision(8) << x[i];
        ofile << setw(15) << setprecision(8) << u[i];
        ofile << setw(15) << setprecision(8) << v[i];
@@ -83,12 +95,10 @@ int main(int argc, char* argv[]) {
     }
     ofile.close();
 
-
     delete [] x;
     delete [] u;
     delete [] v;
     delete [] v_special;
-
 
     return 0;
 }
