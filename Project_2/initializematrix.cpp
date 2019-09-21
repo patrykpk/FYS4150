6753cc8a68223448387e70d72700eb3d60c777e2
Moejay10 <42637454+Moejay10@users.noreply.github.com>
2019-09-21 18:09:33 +0200

    Add files via upload
    Have tried to add the potential to the Toeplitz matrix in a different function.

--- Project_2/initializematrix.cpp
+++ Project_2/initializematrix.cpp
@@ -33,3 +33,41 @@ mat Identity(int n){
 
     return A;
 }
+
+mat Toeplitz_2d(int n){
+
+    mat A = zeros<mat>(n,n);
+
+
+
+    vec rho(n);
+    // start point;
+    rho(0) = 0.0;
+    // End point which is approximated to infinity
+    rho(n-1) = 10.0;
+
+    // Calculating the step size
+    double h = (rho(n-1) - rho(0))/ (double) n;
+
+    for (int i =1; i<n-2; i++)
+    {
+        rho(i) = i*h;
+
+    }
+
+
+    double Diagonal = 2.0/(h*h);
+    double NonDiagonal = -1.0/(h*h);
+
+    //Indexing the missing values at start and end
+    //Since Diagonal contains one more value
+    //A(0,0) = Diagonal; A(0,1) = NonDiagonal;
+    A(n-1,n-1) = Diagonal+rho(n-2)*rho(n-2); A(n-1, n-2) = NonDiagonal;
+
+    for (int i = 1; i < n-1; i++){
+        A(i,i) = Diagonal + rho(i)*rho(i);
+        A(i,i-1) = NonDiagonal;
+        A(i,i+1) = NonDiagonal;
+    }
+    return A;
+}
