ff01f6254f46b4471bd0581e9f87064c09975530
patrykpk <patrykpk@users.noreply.github.com>
2019-10-10 13:54:26 +0200

    Spherical Coordinates and other stuff

---
+++ Branch_Things/repulsion_function.h
@@ -0,0 +1,7 @@
+#ifndef REPULSION_FUNCTION_H
+#define REPULSION_FUNCTION_H
+
+double Cartesian_Function(double x_1, double y_1, double z_1, double x_2, double y_2, double z_2);
+double Spherical_Function(double r_1, double r_2, double theta_1, double theta_2, double phi_1, double phi_2);
+
+#endif // REPULSION_FUNCTION_H
