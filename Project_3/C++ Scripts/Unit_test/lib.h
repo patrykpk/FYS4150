54fcf2633d5d06ec957088671c3b0e9a02478d4e
Marius-619 <54407313+Marius-619@users.noreply.github.com>
2019-10-19 15:56:17 +0200

    Add files via upload

---
+++ Project_3/C++ Scripts/Unit_test/lib.h
@@ -0,0 +1,9 @@
+#ifndef LAGUERRE_H
+#define LAGUERRE_H
+
+void gauss_legendre(double x1, double x2, double x[], double w[], int n);
+void gauss_laguerre(double *x, double *w, int n, double alf);
+double gammln( double xx);
+double ran0(long *idum);
+
+#endif // LAGUERRE_H
