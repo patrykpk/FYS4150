5e8aada920a412f9cab4bcef56a946403491a9c0
patrykpk <patrykpk@users.noreply.github.com>
2019-10-21 22:49:23 +0200

    Removed old stuff

--- Project_3/C++ Scripts/legendre.h
+++
@@ -1,6 +0,0 @@
-#ifndef LEGENDRE_H
-#define LEGENDRE_H
-
-void gauleg(double x1, double x2, double x[], double w[], int n);
-
-#endif // LEGENDRE_H
--- Project_3/C++ Scripts/repulsion_function.h
+++
@@ -1,6 +0,0 @@
-#ifndef REPULSION_FUNCTION_H
-#define REPULSION_FUNCTION_H
-
-double Cartesian_Function(double x_1, double y_1, double z_1, double x_2, double y_2, double z_2);
-
-#endif // REPULSION_FUNCTION_H
